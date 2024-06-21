f35e095c5e78cc73614cd34a4f11da769cbcacae
fmsDesign <brandon300@ymail.com>
2024-06-21 13:07:29 -0300

    projeto Bikcraft

---
+++ js/script.js
@@ -0,0 +1,72 @@
+//link menu
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+//ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//alterar imagem
+
+const galeria = document.querySelectorAll(".nimbus-img img");
+const galeriaConatiner = document.querySelector(".nimbus-img");
+
+function trocarImg(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 950px)").matches;
+
+  if (media) {
+    galeriaConatiner.prepend(img);
+  }
+}
+function eventoClick(img) {
+  img.addEventListener("click", trocarImg);
+}
+
+galeria.forEach(eventoClick);
+
+// Plugin animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
